468fbb01caa218f492bb23508ac8ad3804573697
Azucena Rico Hernandez <marh641@hotmail.com>
2014-06-27 16:25:41 -0600

    primer commit

---
+++ practica1/guardar.php
@@ -0,0 +1,14 @@
+<?php
+echo "Hola......"
+
+echo "Equipo Elegido 1: " .$_POST['selectEquipo1_local'];
+<br/
+
+echo "Goles Equipo 1 Local:" .$_POST['equipo1_local'];
+echo "<br/>";
+echo "Goles Equipo 1 Visitante:" .$_POST['equipo1_visitante'];
+echo "<br/>";
+echo "Goles Equipo 2 Local:" .$_POST['equipo2_local'];
+echo "<br/>";
+echo "Goles Equipo 2 Visitante:" .$_POST['equipo2_visitante'];
+?>
